98614c59042f3e0efa3870e65725f52d971fea0d
Schogan <lrschanbacher@gmail.com>
2018-01-22 19:20:02 -0600

    First Commit

---
+++ homework.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "================================================================\n";
+	cout << "Logan Schanbacher\n";
+	cout << "IT Desktop TEchnician, GC Services - Oklahoma City\n";
+	cout << "\n";
+	cout << "	4316 NW 44th		p: (580) 596-6296\n";
+	cout << "	Oklahoma City, OK	e: Logan.Schanbacher@okstate.edu\n";
+	cout << "				g: github.com/schogan\n";
+	cout << "================================================================";
+	return 0;
+}
